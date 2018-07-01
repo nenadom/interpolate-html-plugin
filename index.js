156b10c2fc771aa6cd466df4174744ef5b99ed01
naterkane <nater.kane@sbd2.com>
2018-07-01 19:59:33 -0400

    check for tapped & undefined * make sure compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing isn't undefined * borrow !tapped++ check from another plugin's webpack4 compatability update

--- index.js
+++ index.js
@@ -13,32 +13,37 @@
 // Learn more about creating plugins like this:
 // https://github.com/ampedandwired/html-webpack-plugin#events
 
-'use strict';
-const escapeStringRegexp = require('escape-string-regexp');
-
+"use strict";
+const escapeStringRegexp = require("escape-string-regexp");
+let tapped = 0;
 class InterpolateHtmlPlugin {
   constructor(replacements) {
     this.replacements = replacements;
   }
 
   apply(compiler) {
-    compiler.hooks.compilation.tap('InterpolateHtmlPlugin', compilation => {
-      compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tap(
-        'InterpolateHtmlPlugin',
-        (data, callback) => {
-          // Run HTML through a series of user-specified string replacements.
-          Object.keys(this.replacements).forEach(key => {
-            const value = this.replacements[key];
-            data.html = data.html.replace(
-              new RegExp('%' + escapeStringRegexp(key) + '%', 'g'),
-              value
-            );
-          });
-          if (typeof callback === 'function') {
-            callback(null, data);
+    compiler.hooks.compilation.tap("InterpolateHtmlPlugin", compilation => {
+      if (
+        !tapped++ &&
+        compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing !== undefined
+      ) {
+        compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tap(
+          "InterpolateHtmlPlugin",
+          (data, callback) => {
+            // Run HTML through a series of user-specified string replacements.
+            Object.keys(this.replacements).forEach(key => {
+              const value = this.replacements[key];
+              data.html = data.html.replace(
+                new RegExp("%" + escapeStringRegexp(key) + "%", "g"),
+                value
+              );
+            });
+            if (typeof callback === "function") {
+              callback(null, data);
+            }
           }
-        }
-      );
+        );
+      }
     });
   }
 }
